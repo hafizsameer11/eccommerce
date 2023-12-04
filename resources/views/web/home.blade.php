ad31e0c6a44581b7dba645dd997fe82a1a052afb
Hafiz <hmstech@gmail.com>
2023-12-04 20:20:55 +0500

    first commit

---
+++ resources/views/web/home.blade.php
@@ -0,0 +1,5 @@
+@include('web.layouts.header')
+@include('web.layouts.navbar')
+
+<h1>Hello wolrd</h1>
+@include('web.layouts.footer')
---
+++ resources/views/web/layouts/header.blade.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+    <link rel="stylesheet" href="{{asset('assets/web/css/animate.css')}}">
+    <link rel="stylesheet" href="{{asset('assets/web/css/bootstrap.min.css')}}">
+    <link rel="stylesheet" href="{{asset('assets/web/css/font-awesome.min.css')}}">
+    <link rel="stylesheet" href="{{asset('assets/web/css/ionicons.min.css')}}">
+    <link rel="stylesheet" href="{{asset('assets/web/css/main.css')}}">
+    <link rel="stylesheet" href="{{asset('assets/web/css/responsive.css')}}">
+    <link rel="stylesheet" href="{{asset('assets/web/css/style.css')}}">
+    {{-- <link rel="stylesheet" href="{{asset('assets/web/css/')}}"> --}}
+    <link rel="stylesheet" href="{{asset('assets/web/rs-plugin/css/settings-ie8.css')}}">
+    <link rel="stylesheet" href="{{asset('assets/web/rs-plugin/css/settings.css')}}">
+    <link rel="stylesheet" href="{{asset('assets/web/rs-plugin/css/settings.css')}}">
+    <link rel="stylesheet" href="{{asset('assets/web/rs-plugin/css/settings.css')}}">
+
+</head>
+<body>
